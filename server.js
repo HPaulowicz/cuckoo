6c88f6b3996eff51a0252b87552f6321eaba95d8
Emma Paulowicz <hpaulowicz@gmail.com>
2021-02-26 00:05:54 +0200

    Working version

--- server.js
+++ server.js
@@ -1,50 +1,57 @@
-const express = require('express')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
-const socket = require('socket.io')
-const io = socket(server)
-const username = require('username-generator')
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+const jwt = require('jsonwebtoken');
 
-const users={}
 
-io.on('connection', socket => {
-    const {
-        handshake: { query: { jwt, callId } }
-    } = socket;
-    console.log(jwt, callId);
-    //generate username against a socket connection and store it
-    const userid=username.generateUsername('-')
-    if(!users[userid]){
-        users[userid] = socket.id
-    }
-    //send back username
-    socket.emit('yourID', userid)
-    io.sockets.emit('allUsers', users)
-    
-    socket.on('disconnect', ()=>{
-        delete users[userid]
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    const { handshake } = socket;
+    console.log('Connected to socket, handshake: ', handshake);
+
+    socket.on('join', (roomId) => {
+        const roomClients = io.sockets.adapter.rooms[roomId] || { length: 0 }
+        const numberOfClients = roomClients.length
+
+        // These events are emitted only to the sender socket.
+        if (numberOfClients == 0) {
+            console.log(`Creating room ${roomId} and emitting room_created socket event`)
+            socket.join(roomId)
+            socket.emit('room_created', roomId)
+        } else if (numberOfClients == 1) {
+            console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+            socket.join(roomId)
+            socket.emit('room_joined', roomId)
+        } else {
+            console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+            socket.emit('full_room', roomId)
+        }
     })
 
-    socket.on('callUser', (data)=>{
-        io.to(users[data.userToCall]).emit('hey', {signal: data.signalData, from: data.from})
+    // These events are emitted to all the sockets connected to the same room except the sender.
+    socket.on('start_call', (roomId) => {
+        console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+        socket.broadcast.to(roomId).emit('start_call')
     })
-
-    socket.on('acceptCall', (data)=>{
-        io.to(users[data.to]).emit('callAccepted', data.signal)
+    socket.on('webrtc_offer', (event) => {
+        console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+        socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp)
     })
-
-    socket.on('close', (data)=>{
-        io.to(users[data.to]).emit('close')
+    socket.on('webrtc_answer', (event) => {
+        console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+        socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp)
     })
-
-    socket.on('rejected', (data)=>{
-        io.to(users[data.to]).emit('rejected')
+    socket.on('webrtc_ice_candidate', (event) => {
+        console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+        socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
     })
 })
 
 const port = process.env.PORT || 8000
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
